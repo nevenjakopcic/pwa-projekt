1be8e8223d1772a5e1155c18480fce90da72ca49
Neven Jakopčić <nevenjakopcic@gmail.com>
2019-06-12 18:49:00 +0200

    Dodani kosturi za Home stranicu i navbar.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>PWA Projekt</title>
+  <link rel="shortcut icon" href="favicon.ico">
+  <link rel="icon" href="favicon.ico">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<header>
+<?php require_once "./nav.php" ?>
+</header>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+	<ul>
+		<li class="home"><a href="index.php">Home</a></li>
+		<li class="vijesti"><a href="vijesti.php">Vijesti</a></li>
+		<li class="muzika"><a href="muzika.php">Muzika</a></li>
+		<li class="admin"><a href="admin.php">Admin</a></li>
+	</ul>
+</nav>
